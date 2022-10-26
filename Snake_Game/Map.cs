43753e28895c7a73bbf2287fa3f921cadf4a08ca
Paradox911 <legoullon.g@gmail.com>
2022-10-26 15:04:16 +0200

    Tentative d'inserer/afficher le tableau

---
+++ Snake_Game/Map.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game
+{
+    class Map
+    {
+        public void main(string[] args)
+        {
+            //Tableau double avec 10 lignes et colonnes.
+            int[,] a = new int[10, 10] { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+            int i, j;
+
+            //Boucle for afin de pouvoir initialiser le tableau
+            for (i = 0; i < 10; i++)
+            {
+
+                for (j = 0; j < 10; j++)
+                {
+                    Console.WriteLine("a[{0},{1}] = {2}", i, j, a[i, j]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+
+
+
+}
--- Snake_Game/Program.cs
+++ Snake_Game/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Snake_Game
+﻿using System;
+using 
+
+
+
+
+namespace Snake_Game
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Debut de jeu
+            Console.WriteLine("Press any key to start");
+            Console.ReadKey();
+            
+            Console.WriteLine("a[{0},{1}] = {2}", i, j, a[i, j]);
+
+
+
         }
     }
 }
