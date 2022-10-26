3ba49366faeb602ef77661ebd36f31ed43708bfc
Paradox911 <legoullon.g@gmail.com>
2022-10-26 16:08:57 +0200

    Addition de la Classe Movement

---
+++ Snake_Game/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game
+{
+	internal class Movement
+	{
+		char c;
+		int x;
+		int y;
+
+        //switch (c)
+
+        //X et Y sont ici inverse, car l'affichage est lui même inverse
+        /*case UP:
+				x--;
+			break;
+		case DOWN:
+				x++;
+			break;
+		case LEFT:
+				y--;
+			break;
+		case RIGHT:
+				y++;
+			break;
+			//Si on appuye sur escape, on demande au jeu de se terminer.
+		default:
+			break;
+			}*/
+
+    }
+}
+
